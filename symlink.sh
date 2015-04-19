2cbdb39b799dc1e14b28ed32f736868607dabf7d
@greglgomez <greglgomez@gmail.com>
2015-04-19 17:42:13 +0100

    created symlink script for dotfiles

---
+++ symlink.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# symlinks all dotfiles to ~
+
+ln -s ~/code/dotfiles/.bash_aliases 	~/.bash_aliases
+ln -s ~/code/dotfiles/.bash_profile 	~/.bash_profile
+ln -s ~/code/dotfiles/.bashrc 			~/.bashrc
+ln -s ~/code/dotfiles/.gitconfig 		~/.gitconfig
+ln -s ~/code/dotfiles/.gitignore 		~/.gitignore
+ln -s ~/code/dotfiles/.hushlogin 		~/.hushlogin
